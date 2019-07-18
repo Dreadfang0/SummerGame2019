6e7fc05fb79eb4a186afbd3792c12d77b6a4b7d1
Petlin Saku TTK18SP <sakupetlin@kamk.fi>
2019-07-18 13:42:07 +0300

    boss portal sound added

--- Roguelite/Assets/Scripts/StarRoomPortal.cs
+++ Roguelite/Assets/Scripts/StarRoomPortal.cs
@@ -12,18 +12,11 @@ public class StarRoomPortal : MonoBehaviour
     public GameObject ParentRoom;
     public PlayerController playerController;
 
-    // Start is called before the first frame update
     void Start()
     {
         Vector3 rotationVector = new Vector3(0, 30, 0);
         Quaternion rotation = Quaternion.Euler(0, 30, 0);
         playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     void OnTriggerEnter(Collider other)
